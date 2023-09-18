d5094dd571503d3f9145c160ca8ed0f2df3606d0
MERN.Kishan Kalathia <kishan.kalathia@webcluesinfotech.com>
2023-09-18 10:12:33 +0530

    Book Added

---
+++ src/book/book.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Get,
+  Controller,
+  Post,
+  Body,
+  Param,
+  Put,
+  Delete,
+} from '@nestjs/common';
+import { BookService } from './book.service';
+import { Book } from './schema/book.schema';
+import { CreateBookDto } from './dto/create-book-dto';
+
+@Controller('book')
+export class BookController {
+  constructor(private bookService: BookService) {}
+
+  @Get()
+  async getAllBooks(): Promise<Book[]> {
+    return this.bookService.findAll();
+  }
+
+  @Post()
+  async createBook(@Body() book: CreateBookDto): Promise<Book> {
+    return this.bookService.create(book);
+  }
+
+  @Get(':id')
+  async getBook(@Param('id') id: string): Promise<Book> {
+    return this.bookService.findById(id);
+  }
+
+  @Put(':id')
+  async updateBookById(
+    @Param('id') id: string,
+    @Body() book: CreateBookDto,
+  ): Promise<Book> {
+    return this.bookService.updateById(id, book);
+  }
+
+  @Delete(':id')
+  async deleteBook(@Param('id') id: string): Promise<Book> {
+    return this.bookService.deleteById(id);
+  }
+}
